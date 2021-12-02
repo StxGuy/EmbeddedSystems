7ef25299b8787be5483c37d46b178979b3c1fda3
Carlo R. da Cunha <carlo.requiao@gmail.com>
2021-12-02 19:39:53 -0300

    Create indirect_addressing2.asm

---
+++ AVR/indirect_addressing2.asm
@@ -0,0 +1,31 @@
+;-------------------------------------;
+; INDIRECT ADDRESSING FOR ATMEL328    ;
+;                                     ;
+; Por: Prof. Carlo Requiao            ;
+; 16/Sep/2020                         ;
+;-------------------------------------;
+
+.device ATmega328
+
+.dseg
+    A: .byte 0x01
+    
+.cseg    
+.org 0x00   ; Program starts at 0x00
+
+rjmp INICIO
+
+
+INICIO:
+    ldi R16, 0x04
+    ldi R26, low(A)
+    ldi R27, high(A)
+    
+    st  X, R16
+    ld  R17, X
+
+LOOP:
+  jmp LOOP
+
+
+          
