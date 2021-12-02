d3e2604a06b2444607d743d76db9be1ff09eebdc
Carlo R. da Cunha <carlo.requiao@gmail.com>
2021-12-02 19:36:50 -0300

    Create copy_array.asm

---
+++ AVR/copy_array.asm
@@ -0,0 +1,55 @@
+;-------------------------------------;
+; STACK FOR ATMEL328                  ;
+;                                     ;
+; Por: Prof. Carlo Requiao            ;
+; 22/Sep/2020                         ;
+;-------------------------------------;
+
+.device ATmega328
+
+.macro initstack
+	ldi	R16,low(RAMEND)
+	out	spl,R16
+	ldi	R16,high(RAMEND)
+	out	sph,R16
+.endmacro
+
+.def geral1=R18
+.def geral2=R19
+
+.dseg
+   dest: .byte 0x05
+
+.cseg
+
+.org 0x00   ; Program starts at 0x00
+
+rjmp INICIO
+
+
+INICIO:
+    initstack
+    ldi		geral1,5
+    ldi		ZL,low(array<<1)
+    ldi		ZH,high(array<<1)
+PUT:
+    lpm		R1,Z+
+    push	R1
+    dec		geral1
+    brne	PUT
+
+    ldi		geral1,5
+    ldi		XL,low(dest)
+    ldi		XH,high(dest)
+TAKE:
+    pop		R1
+    st		X+,r1
+    dec		geral1
+    brne	TAKE
+
+
+LOOP:
+    jmp     LOOP
+
+array: .db 1,2,3,4,5
+
